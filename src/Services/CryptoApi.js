8d94477d49ae4027e95b4114996bff82f3840d88
Rafi Ahmed <rafiahmed0471@gmail.com>
2022-01-31 06:58:22 +0600

    done with news and cryptoCurrencie

--- src/Services/CryptoApi.js
+++ src/Services/CryptoApi.js
@@ -1,9 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const cryptoApiHeaders = {
-    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-    'x-rapidapi-key': '95ef68e9d2mshbd4d35bdd1948e0p13ba37jsn06bceea9e6d3'
-}
+
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
 
--- src/Services/CryptoNewsApi.js
+++ src/Services/CryptoNewsApi.js
@@ -1,10 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const CryptoNewsApiHeaders = {
-    'x-bingapis-sdk': 'true',
-    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
-    'x-rapidapi-key': '95ef68e9d2mshbd4d35bdd1948e0p13ba37jsn06bceea9e6d3'
-}
+
 
 const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
 
